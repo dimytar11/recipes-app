0679078eb697039d9aa632d6880a184e8a89d808
Dimitar Dimitrov <dimytar11@gmail.com>
2022-12-06 16:25:10 +0200

    passed an array into recipe-list component, recipe-detail component is changed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,8 +10,18 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
       'Burger',
-      'Ham, cheese, salad',
-      'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p.jpg'
+      'ham, cheese, salad',
+      'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/master/pass/Smashburger-recipe-120219.jpg'
+    ),
+    new Recipe(
+      'Pizza',
+      'ham, cheese, tomatoes',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg'
+    ),
+    new Recipe(
+      'Chicken soup',
+      'chicken, potatoes, carrots, onion',
+      'https://www.seriouseats.com/thmb/2nouHHsjM0bN1vwXMOZGUkLFsJ8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2017__12__20171115-chicken-soup-vicky-wasik-11-80db1a04d84a43a089e0559efdddd517.jpg'
     ),
   ];
 
