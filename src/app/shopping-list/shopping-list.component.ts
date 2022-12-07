b5946a9cdf18b8f287ff303de48290ade87a6b79
Dimitar Dimitrov <dimytar11@gmail.com>
2022-12-07 08:51:34 +0200

    shopping-list template is created

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
+import { Ingredient } from './../model/ingredient';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Ham', 5),
+    new Ingredient('Cheese', 10),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
